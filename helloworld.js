a86521778241072d61a279298342ea3f35374751
Herculano <lucasgherculano@gmail.com>
2020-07-06 21:40:25 -0300

    jenkins is not easy as i think

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello Beautiful World\n')
+   response.end('Hello Beautiful New World\n')
 }).listen(3000)
 
 // Console will print the message
